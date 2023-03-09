a6d002043039d28dee51b1a0d0673acab436abae
SoraJang1 <ohaya@naver.com>
2023-03-09 21:34:10 +0900

    first rev

--- src/BaekJoon01/Test01_01_2557.java
+++ src/BaekJoon01/Test01_01_2557.java
@@ -4,8 +4,6 @@ public class Test01_01_2557 {
 	
 	public static void main (String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("Hello World!");
-		System.out.println("first githug commint!");
 	}
 
 }
