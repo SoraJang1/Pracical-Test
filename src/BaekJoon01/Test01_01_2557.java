54d3aca5ef306501c1ca5549d4d439ae2e4f1b01
SoraJang1 <ohaya@naver.com>
2023-03-09 20:42:17 +0900

    first github commit, please

---
+++ src/BaekJoon01/Test01_01_2557.java
@@ -0,0 +1,11 @@
+package BaekJoon01;
+
+public class Test01_01_2557 {
+	
+	public static void main (String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("first githug commint!");
+	}
+
+}
